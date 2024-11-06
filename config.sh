13d2d2803fa98210cbea15524a372f883d14d8b3
Luka Capan <91406500+R5C-V15T6AMD5S@users.noreply.github.com>
2024-11-06 11:22:54 +0100

    Overhaul of the scripts
    And added a new option to enable sound in TempleOS

--- config.sh
+++ config.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 if [ -z ${QEMU_HOME+x} ]; then
     QEMU_SYSTEM_X86_64=qemu-system-x86_64
     QEMU_IMG=qemu-img
@@ -6,7 +8,7 @@ else
     QEMU_IMG=$QEMU_HOME/bin/qemu-img
 fi
 
-QEMU_FLAGS="-display gtk,zoom-to-fit=on -enable-kvm -m 4096"
+QEMU_FLAGS="-display gtk,zoom-to-fit=on -enable-kvm -m 4096 -audiodev pa,id=audio0 -machine pcspk-audiodev=audio0"
 QEMU_IMG_SIZE="512M"
 QEMU_IMG_MOUNT_DIR="./mnt/"
 # Note on the offset: https://www.cloudsavvyit.com/7517/how-to-mount-a-qemu-virtual-disk-image/
