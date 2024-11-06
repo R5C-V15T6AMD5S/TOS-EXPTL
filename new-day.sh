53ad6bb67b86451e2979dbaef4f64169b40ca11c
Luka Capan <91406500+R5C-V15T6AMD5S@users.noreply.github.com>
2024-11-06 11:23:46 +0100

    Added script NEW-DAY
    To create a folder inside AOC-2024 with a name of the day.

---
+++ new-day.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+if [ $# -lt 2 ]; then
+    echo "Usage: ./new-day.sh <TOS.img> <folder>"
+    echo "ERROR: not enough arguments is provided"
+    exit 1
+fi
+
+set -xe
+
+. ./config.sh
+
+./mount.sh "$1"
+
+cd "$QEMU_IMG_MOUNT_DIR/Home/AOC-2024/"
+mkdir -p "$2"
+cp "./Template.HC" "$2/Main.HC"
+ed "$2/sample.TXT"
+ed "$2/input.TXT"
+
