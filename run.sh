1cfe507182ac087c29fa98c623cad0bbd01e43bb
Luka Capan <91406500+R5C-V15T6AMD5S@users.noreply.github.com>
2025-01-17 22:05:07 +0100

    Update run.sh
    Added a share_iso.sh if in need.

--- run.sh
+++ run.sh
@@ -9,7 +9,17 @@ fi
 set -xe
 
 . ./config.sh
+#. ./share_iso.sh
 
 sync
 sudo umount $QEMU_IMG_MOUNT_DIR || true
-$QEMU_SYSTEM_X86_64 $QEMU_FLAGS "$1"
+
+# Define paths to supplemental ISOs
+SUPPLEMENTAL1_PATH="/home/o5-14/VirtualBox/TempleOS/shr/TOS_Supplemental1.ISO.C"  # Utilities & Games
+SUPPLEMENTAL2_PATH="/home/o5-14/VirtualBox/TempleOS/shr/TOS_Supplemental2.ISO.C"  # Generated Songs Pt. 1
+SUPPLEMENTAL3_PATH="/home/o5-14/VirtualBox/TempleOS/shr/TOS_Supplemental3.ISO.C"  # Generated Songs Pt. 2
+
+$QEMU_SYSTEM_X86_64 $QEMU_FLAGS \
+    -drive file="$1",format=raw \
+    -cdrom "$TEMPLEOS_ISO" \
+    -drive file="$SUPPLEMENTAL1_PATH",media=cdrom 
